7cfc93e3d31789fa8523077405563b999df75962
jphayek <jphayek1@gmail.com>
2024-04-24 20:05:56 +0200

    LIKE Button

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,47 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
-import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+import {ActionMessage} from "@workadventure/iframe-api-typings";
 
 console.log('Script started successfully');
 
-let currentPopup: any = undefined;
-
 // Waiting for the API to be ready
 WA.onInit().then(() => {
-    console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
+  let actionMessage: ActionMessage | undefined;
+  let likeCount = 0;
+  // When someone enters the bellZone area
 
-    WA.room.area.onEnter('clock').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
-    })
+  let stopLike = false;
 
-    WA.room.area.onLeave('clock').subscribe(closePopup)
+  WA.room.area.onEnter("bellZone").subscribe(() => {
+    console.log("oui");
+    
+    // Display the action message
+    function genMessage() {
+        actionMessage = WA.ui.displayActionMessage({
+        type: "message",
+        message: "Press SPACE to LIKE",
+        callback: () => {
+            likeCount++;
+            // When space is pressed, we send a "bell-rang" signal to everyone on the map.
+            console.log(`*****${actionMessage} ${likeCount}******`);
 
-    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
-    bootstrapExtra().then(() => {
-        console.log('Scripting API Extra ready');
-    }).catch(e => console.error(e));
+            genMessage();
+        }
+        });
+    }
+
+    genMessage();    
+  });
+
+  // When someone leaves the bellZone area
+  WA.room.area.onLeave("bellZone").subscribe(() => {
+    if (actionMessage !== undefined) {
+      // Hide the action message
+      actionMessage.remove();
+      actionMessage = undefined;
+    }
+  });
 
 }).catch(e => console.error(e));
-
-function closePopup(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
 
 export {};
