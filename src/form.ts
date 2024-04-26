ab841e5154e002f06cd9fc54215ed8fcb3d9e96a
Estelle <estelle272001@gmail.com>
2024-04-26 06:18:16 +0200

    succes js

--- src/form.ts
+++ src/form.ts
@@ -1,14 +1,15 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
 
+
 console.log('Script started successfully');
 
-const noteTextArea = document.getElementById("noteTextArea") as HTMLTextAreaElement;
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
 
-    noteTextArea.value = (WA.state.noteText ?? "") as string;
+    console.log("ROOM CREEATED..");
+    WA.nav.goToRoom('conference');
 
 }).catch(e => console.error(e));
 
