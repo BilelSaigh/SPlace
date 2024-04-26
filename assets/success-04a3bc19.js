f18f795d782d92b9cfaed89875a5862167ee93de
Stelleu <72598682+Stelleu@users.noreply.github.com>
2024-04-26 11:48:02 +0000

    Deploying to gh-pages from master @ adee624ae4a5ddbdd8f5da6bb4762c47cd158095 🚀

--- assets/success-04a3bc19.js
+++
@@ -1,1 +0,0 @@
-import"./modulepreload-polyfill-3cfb730f.js";console.log("Script started successfully");WA.onInit().then(()=>{console.log("ROOM CREEATED.."),WA.nav.goToRoom("conference")}).catch(o=>console.error(o));
