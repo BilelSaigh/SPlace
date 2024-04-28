1e4345cf7692787b74c133f3a16cbdeedcc4e7b9
Bilel SAIGH <bilelsaigh94@gmail.com>
2024-04-28 20:14:13 +0200

    .

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,92 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
-import { bootstrapExtra } from "@workadventure/scripting-api-extra";
-
+import {RemotePlayer} from "@workadventure/iframe-api-typings/play/src/front/Api/Iframe/Players/RemotePlayer";
+import {bootstrapExtra} from "@workadventure/scripting-api-extra/dist";
 
 console.log('Script started successfully');
 
 let currentPopup: any = undefined;
 
 // Waiting for the API to be ready
-WA.onInit().then(() => {
-    console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags);
+WA.onInit().then(  async () => {
+    await WA.player.state.saveVariable("tags", WA.player.tags, {
+        public: true,
+        persist: true,
+        scope: "world",
+    });
+
+    WA.ui.actionBar.addButton({
+        id: 'donate-btn',
+        label: 'Donate',
+        callback: (event) => {
+            console.log('Button clicked', event);
+            // When a user clicks on the action bar button 'Register', we remove it.
+            WA.ui.actionBar.removeButton('register-btn');
+        }
+    });
+
+    let noteWebsite: any;
+    WA.ui.onRemotePlayerClicked.subscribe( async (remotePlayer: RemotePlayer)  => {
+        console.log(remotePlayer.state.tags)
+        let tags = remotePlayer.state.tags as string[];
+        //verifier si le joueur est un model pour afficher les informations
+        if (tags.includes('model')) {
+            await WA.players.configureTracking();
+            const player = WA.players.get(remotePlayer.playerId);
+            if (player !== undefined) {
+                remotePlayer.addAction('En savoir plus', async () => {
+                    await WA.player.state.saveVariable("clickID", player.playerId, {
+                        public: true,
+                        persist: true,
+                        scope: "world",
+                    });
+                    noteWebsite = await WA.ui.website.open({
+                        url: `./form.html?param=${encodeURIComponent(player.name)}`,
+                        position: {
+                            vertical: "middle",
+                            horizontal: "middle",
+                        },
+                        size: {
+                            height: "30vh",
+                            width: "80vh",
+                        },
+                        margin: {
+                            top: "10vh",
+                        },
+                        allowApi: true,
+                    })
+                    setTimeout(() => {
+                        noteWebsite.close();
+                        }   , 5000)
+                })
+            }
+
+        }
+    });
+
+
+    WA.room.onEnterLayer("formLayer").subscribe(async () => {
+        noteWebsite = await WA.ui.website.open({
+            url: "./form.html",
+            position: {
+                vertical: "top",
+                horizontal: "middle",
+            },
+            size: {
+                height: "30vh",
+                width: "80vh",
+            },
+            margin: {
+                top: "10vh",
+            },
+            allowApi: true,
+        });
+    });
+    WA.room.onLeaveLayer("formLayer").subscribe(() => {
+        noteWebsite.close();
+    });
+
+
 
 
     WA.room.area.onEnter('clock').subscribe(() => {
@@ -19,8 +95,10 @@ WA.onInit().then(() => {
         currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
     })
 
+
     WA.room.area.onLeave('clock').subscribe(closePopup)
 
+<<<<<<< HEAD
 
     let noteWebsite: any;
 
@@ -50,20 +128,12 @@ WA.onInit().then(() => {
     });
 
 
+=======
+>>>>>>> 7071671 (.)
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
         console.log('Scripting API Extra ready');
     }).catch(e => console.error(e));
-
-
-
-    // If the player enters the room between 19:00 and 7:00, hide the laptops
-    const now = new Date();
-    const startNight = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 19, 0, 0);
-    const startDay = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 7, 0, 0);
-    if (now > startNight || now < startDay) {
-        WA.room.hideLayer("above/laptops");
-    }
 
 }).catch(e => console.error(e));
 
