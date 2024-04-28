9b2c79c1c617af662a35d0290ece8f7746646f5e
BilelSaigh <70441190+BilelSaigh@users.noreply.github.com>
2024-04-28 22:19:01 +0000

    Deploying to gh-pages from master @ dc274e96ffd3c0c3a82f50dc23eb6bcdc0d97217 🚀

---
+++ assets/form-ce0a23de.js
@@ -0,0 +1,4 @@
+console.log("Script started successfully");WA.onInit().then(async()=>{let e=window.location.hash,s=decodeURIComponent(e.split("=")[1]),t=document.querySelector(".card-title"),o=document.querySelector(".card-text");t.textContent=s,o.textContent=`Bonjour, je suis ${s}, une experte spécialisée dans les jeux de domination et de soumission, ainsi que dans le sadomasochisme. 
+                            Avec moi, plonges dans un univers où les limites sont repoussées et les désirs les plus sombres explorés.
+                            Que tu cherches l'excitation des insultes cinglantes, la délicieuse torture du sadomasochisme ou la soumission totale à mes ordres, je suis là pour t'offrir une expérience inoubliable et intense. 
+                            Oserais-tu t'aventurer dans mes jeux de pouvoir ?`}).catch(e=>console.error(e));
